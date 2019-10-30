2effe76260036fe3c2123576f438e86dc0185c80
Bayron Puac <bayronpuac@gmail.com>
2019-10-30 12:34:12 -0500

    I think I missed something.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -10,14 +11,36 @@ function dataLogger(req, res, next) {
 
   next();
 }
-function html(req, res, next) {
-  
+function logger(req, res, next) {
+  console.log(`The Logger: [${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next();
 }
 
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+  if( !password === 0) {
+    res.status(400).json({
+      error: "please provide a password"
+    })
+  } else if(password.toLowerCase() === 'mellon'){
+    next();
+  } else {
+    res.status(401).json({
+      you: 'cannot pass!!'
+    })
+  }
+}
+
+
 // Global middleware
+
 server.use(helmet());
 server.use(express.json()); // built-in 
-server.use(dataLogger);
+server.use(gateKeeper);
+// server.use(dataLogger);
+server.use(logger);
+server.use(morgan('dev'));
 
 server.use('/api/hubs', hubsRouter);
 
